82f0e4a119036f4ade83baed8aa8d77a9eec8749
Halimao <1065621723@qq.com>
2024-02-01 22:19:35 +0800

    first init

---
+++ cmd/main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"bufio"
+	"crypto"
+	"crypto/ed25519"
+	"crypto/rand"
+	"encoding/base32"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+
+	file_transfer "github.com/Halimao/file-transfer"
+	"github.com/libp2p/go-libp2p/core/peer"
+	ma "github.com/multiformats/go-multiaddr"
+)
+
+func main() {
+	privKey, err := getPrivKey(".key")
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	dirPath := flag.String("dir", "data", "directory to serve")
+	port := flag.String("port", "9700", "port for the server")
+	newKey := flag.Bool("newKey", false, "should use new key for ID")
+	flag.Parse()
+
+	opts := []file_transfer.Option{file_transfer.WithPort(*port)}
+	if !*newKey {
+		opts = append(opts, file_transfer.WithPrivKey(privKey))
+	}
+	n, err := file_transfer.NewNode(*dirPath, opts...)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+OUTER:
+	for {
+		r := bufio.NewScanner(os.Stdin)
+		for r.Scan() {
+			line := r.Text()
+			words := strings.Split(line, " ")
+			switch strings.ToLower(words[0]) {
+			case "list":
+				id, err := peer.Decode(words[1])
+				if err != nil {
+					fmt.Println(err)
+					continue OUTER
+				}
+				addr := ma.StringCast(words[2])
+				fmt.Println(n.GetFileList(peer.AddrInfo{ID: id, Addrs: []ma.Multiaddr{addr}}))
+			case "get":
+				id, err := peer.Decode(words[2])
+				if err != nil {
+					fmt.Println(err)
+					continue OUTER
+				}
+				addr := ma.StringCast(words[3])
+				fmt.Println(string(n.GetFile(peer.AddrInfo{ID: id, Addrs: []ma.Multiaddr{addr}}, words[1])))
+			case "addrs":
+				fmt.Println(n.ID())
+				for _, addr := range n.Addrs() {
+					fmt.Println(addr.Encapsulate(ma.StringCast("/p2p/" + n.ID().String())))
+				}
+			}
+		}
+	}
+}
+
+func getPrivKey(path string) (crypto.PrivateKey, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return newPrivKey(path)
+	}
+	defer f.Close()
+
+	bk, err := io.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+	pkb, err := base32.StdEncoding.DecodeString(string(bk))
+	if err != nil {
+		return nil, err
+	}
+	pk := ed25519.PrivateKey(pkb)
+	return &pk, nil
+}
+
+func newPrivKey(path string) (crypto.PrivateKey, error) {
+	f, err := os.Create(path)
+	if err != nil {
+		return nil, err
+	}
+	_, privKey, err := ed25519.GenerateKey(rand.Reader)
+	if err != nil {
+		return nil, err
+	}
+	_, err = f.WriteString(base32.StdEncoding.EncodeToString(privKey))
+	if err != nil {
+		f.Close()
+		os.Remove(path)
+		return nil, err
+	}
+	return privKey, nil
+}
---
+++ node.go
@@ -0,0 +1,303 @@
+package file_transfer
+
+import (
+	"bufio"
+	"context"
+	"crypto"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path"
+	"sort"
+	"time"
+
+	"github.com/libp2p/go-libp2p"
+	dht "github.com/libp2p/go-libp2p-kad-dht"
+	lcrypto "github.com/libp2p/go-libp2p/core/crypto"
+	"github.com/libp2p/go-libp2p/core/event"
+	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/peerstore"
+	ma "github.com/multiformats/go-multiaddr"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+const (
+	ListProtocol = "/halimao/file/list"
+	GetProtocol  = "/halimao/file/fetch"
+	peerChanSize = 100
+)
+
+type Node struct {
+	privKey lcrypto.PrivKey
+	host    host.Host
+	dht     *dht.IpfsDHT
+	ctx     context.Context
+	cancel  func()
+
+	dirPath string
+}
+
+type Config struct {
+	privKey lcrypto.PrivKey
+	pubKey  lcrypto.PubKey
+	port    string
+}
+
+type Option func(c *Config) error
+
+func WithPrivKey(pk crypto.PrivateKey) Option {
+	return func(c *Config) error {
+		var err error
+		c.privKey, c.pubKey, err = lcrypto.KeyPairFromStdKey(pk)
+		return err
+	}
+}
+
+func WithPort(p string) Option {
+	return func(c *Config) error {
+		c.port = p
+		return nil
+	}
+}
+
+func NewNode(dirPath string, opts ...Option) (*Node, error) {
+
+	cfg := Config{
+		port: "9700",
+	}
+	for _, o := range opts {
+		if err := o(&cfg); err != nil {
+			return nil, err
+		}
+	}
+
+	peerChan := make(chan peer.AddrInfo, 100)
+	lOpts := []libp2p.Option{
+		libp2p.ListenAddrStrings(fmt.Sprintf("/ip4/0.0.0.0/udp/%s/quic-v1", cfg.port)),
+		libp2p.EnableAutoRelayWithPeerSource(
+			func(ctx context.Context, num int) <-chan peer.AddrInfo {
+				ch := make(chan peer.AddrInfo, num)
+
+				go func() {
+					ctxDone := false
+					for i := 0; i < num; i++ {
+						select {
+						case ai := <-peerChan:
+							ch <- ai
+						case <-ctx.Done():
+							ctxDone = true
+						}
+						if ctxDone {
+							break
+						}
+					}
+					close(ch)
+				}()
+				return ch
+			},
+		),
+		libp2p.EnableHolePunching(),
+	}
+
+	if cfg.privKey != nil {
+		lOpts = append(lOpts, libp2p.Identity(cfg.privKey))
+	}
+
+	h, err := libp2p.New(lOpts...)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	go relayProvider{host: h}.Provide(ctx, peerChan)
+
+	d, err := dht.New(ctx, h,
+		dht.BootstrapPeers(dht.GetDefaultBootstrapPeerAddrInfos()...),
+		dht.Mode(dht.ModeClient),
+	)
+	if err != nil {
+		cancel()
+		return nil, err
+	}
+	if err := d.Bootstrap(ctx); err != nil {
+		cancel()
+		return nil, err
+	}
+
+	go func() {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Print(err)
+			}
+		}()
+		http.Handle("/metrics", promhttp.Handler())
+		http.ListenAndServe("0.0.0.0:9600", nil)
+	}()
+
+	n := &Node{
+		host:    h,
+		dht:     d,
+		ctx:     ctx,
+		cancel:  cancel,
+		dirPath: dirPath,
+	}
+	n.host.SetStreamHandler(ListProtocol, n.handleFileList)
+	n.host.SetStreamHandler(GetProtocol, n.sendFile)
+	return n, err
+}
+
+func (n *Node) Addrs() []ma.Multiaddr {
+	return n.host.Addrs()
+}
+
+func (n *Node) NumPeers() int {
+	return len(n.host.Network().Peers())
+}
+
+func (n *Node) ID() peer.ID {
+	return n.host.ID()
+}
+
+func (n *Node) Close() {
+	n.cancel()
+	n.host.Close()
+	n.dht.Close()
+}
+
+func (n *Node) FileNames() ([]string, error) {
+	entries, err := os.ReadDir(n.dirPath)
+	if err != nil {
+		return nil, fmt.Errorf("read dir failed: %w", err)
+	}
+	files := make([]string, 0, len(entries))
+	for _, e := range entries {
+		if !e.IsDir() {
+			files = append(files, e.Name())
+		}
+	}
+	sort.Strings(files)
+	return files, nil
+}
+
+func (n *Node) handleFileList(s network.Stream) {
+	f, err := n.FileNames()
+	if err != nil {
+		s.Reset()
+		return
+	}
+	b, err := json.Marshal(f)
+	if err != nil {
+		s.Reset()
+		return
+	}
+	if _, err := s.Write(b); err != nil {
+		s.Reset()
+		return
+	}
+	s.Close()
+}
+
+func (n *Node) GetFileList(ai peer.AddrInfo) []string {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+	ctx = network.WithUseTransient(ctx, "fetch file list")
+
+	n.host.Peerstore().AddAddrs(ai.ID, ai.Addrs, peerstore.TempAddrTTL)
+	s, err := n.host.NewStream(ctx, ai.ID, ListProtocol)
+	if err != nil {
+		log.Printf("%s open stream failed %s: %s\n", ListProtocol, ai, err)
+		return nil
+	}
+	var ss []string
+	if err := json.NewDecoder(s).Decode(&ss); err != nil {
+		log.Printf("error reading file list %s: %s\n", ai, err)
+	}
+	return ss
+}
+
+func (n *Node) sendFile(s network.Stream) {
+	defer s.Close()
+
+	if s.Stat().Transient {
+		log.Println("cannot send file over transient connection", s.Conn().RemotePeer())
+		s.Reset()
+		return
+	}
+
+	var name string
+	if err := json.NewDecoder(s).Decode(&name); err != nil {
+		log.Println("invalid file name: ", err)
+		s.Reset()
+		return
+	}
+
+	f, err := os.Open(path.Join(n.dirPath, name))
+	if err != nil {
+		log.Println("couldn't open file", name, err)
+		s.Reset()
+		return
+	}
+
+	_, err = io.Copy(s, bufio.NewReader(f))
+	if err != nil {
+		s.Reset()
+		return
+	}
+}
+
+func (n *Node) GetFile(ai peer.AddrInfo, file string) []byte {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	n.host.Peerstore().AddAddrs(ai.ID, ai.Addrs, peerstore.TempAddrTTL)
+	s, err := n.host.NewStream(ctx, ai.ID, GetProtocol)
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+
+	b, err := json.Marshal(file)
+	if err != nil {
+		s.Reset()
+		return nil
+	}
+	b = append(b, byte('\n'))
+	if _, err := s.Write(b); err != nil {
+		s.Reset()
+		return nil
+	}
+
+	res, err := io.ReadAll(s)
+	if err != nil {
+		return nil
+	}
+	return res
+}
+
+type relayProvider struct {
+	host host.Host
+}
+
+func (r relayProvider) Provide(ctx context.Context, peerChan chan peer.AddrInfo) {
+	sub, err := r.host.EventBus().Subscribe(new(event.EvtPeerIdentificationCompleted))
+	if err != nil {
+		log.Printf("subscription failed: %s", err)
+		return
+	}
+	for {
+		select {
+		case e, ok := <-sub.Out():
+			if !ok {
+				return
+			}
+			evt := e.(event.EvtPeerIdentificationCompleted)
+			peerChan <- peer.AddrInfo{ID: evt.Peer}
+		case <-ctx.Done():
+			return
+		}
+	}
+}
